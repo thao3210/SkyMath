2a9cba3e7143cb96c9ea13599a700330414751e1
thao <nguyenthao21304@gmail.com>
2024-03-20 01:06:20 +0700

    update deploy

--- FE/src/layout/contact/Contact.js
+++ FE/src/layout/contact/Contact.js
@@ -8,14 +8,16 @@ const Contact = () => {
     return (
         <section>
             <div className="d-flex flex-column align-items-end gap-1 position-fixed z-3 contact-section">
-                <FacebookMsg />
-
                 <button className="btn-contact p-0 border-0">
                     <img src={Zalo} alt="zalo" className="w-100"></img>
                 </button>
-                <a href="tel:0347225127" className="btn-contact p-0 border-0 fa-shake">
+                <a href="tel:0332331829" className="btn-contact p-0 border-0 fa-shake">
                     <img src={Phone} alt="phone" className="w-100"></img>
                 </a>
+                <button>haha</button>
+                <FacebookMsg />
+
+
             </div>
         </section>
     );
